e21a56ec304df3b27de059b36728ea0bd9110b73
AitaHaruto <aitaharuto2@gmailcom>
2026-07-11 18:56:57 +0900

    モック画面の作成

--- intern-book-app/src/app/app.routes.ts
+++ intern-book-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MockComponent } from './pages/mock/mock.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MockComponent } // ← ここが path: '' になっていること
+];
