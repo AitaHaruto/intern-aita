df634b267eb971081bf588ee456f89fc5db4ce1e
Kazuya Hayashi <>
2026-07-22 18:23:51 +0900

    参考までに

--- intern-book-app/src/app/app.component.ts
+++ intern-book-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { book } from './types/book/book.component';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'intern-book-app';
+  
+  bookList: book[] = [
+    {
+      name: 'アンドロイドは電気羊の夢を見るか？',
+      detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが、火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ',
+      evaluation: 90,
+    },
+    {
+      name: '岩田さん：岩田聡はこんなことを話していた。',
+      detail: '任天堂の元社長、岩田聡さんのことばをまとめた本',
+      evaluation: 90,
+
+    }
+  ];
 }
